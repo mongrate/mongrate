27dd4621f7fa40c95b4cb026fbe6a75fc4479f36
Amy Boyd <amy@amyboyd.co.uk>
2014-07-03 11:14:20 +0100

    Allow command params to be set through constructor.

--- src/Mongrate/Command/BaseCommand.php
+++ src/Mongrate/Command/BaseCommand.php
@@ -9,11 +9,20 @@ class BaseCommand extends Command
 {
     protected $params;
 
-    public function __construct($name = null)
+    /**
+     * @param string $name   Optional.
+     * @param array  $params Optional. Can be used by wrappers like MongrateBunde (Symfony).
+     *                       If not set, reads the params from 'config/parameters.yml'.
+     */
+    public function __construct($name = null, array $params = null)
     {
         parent::__construct($name);
 
-        $yaml = new Parser();
-        $this->params = $yaml->parse(file_get_contents('config/parameters.yml'))['parameters'];
+        if (is_array($params)) {
+            $this->params = $params;
+        } else {
+            $yaml = new Parser();
+            $this->params = $yaml->parse(file_get_contents('config/parameters.yml'))['parameters'];
+        }
     }
 }
