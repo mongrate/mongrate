7273ba5991cf78414e5215fbee47fccdbb2b3b6b
Amy Boyd <amy@amyboyd.co.uk>
2015-08-21 07:57:46 +0100

    Add name validation: must be valid characters and not empty.

---
+++ test/Mongrate/Tests/Migration/NameTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Mongrate\Tests\Migration;
+
+use Mongrate\Migration\Name;
+
+class NameTest extends \PHPUnit_Framework_TestCase
+{
+    public function testValidNameDoesNotThrowException()
+    {
+        new Name('AValidName');
+    }
+
+    /**
+     * @expectedException \Mongrate\Exception\InvalidNameException
+     * @expectedExceptionMessage contains invalid characters: £"(
+     */
+    public function testThrowsExceptionIfContainsInvalidCharacters()
+    {
+        new Name('A£b"l(');
+    }
+
+    /**
+     * @expectedException \Mongrate\Exception\InvalidNameException
+     * @expectedExceptionMessage must not be empty
+     */
+    public function testThrowsExceptionIfEmpty()
+    {
+        new Name('');
+    }
+
+    /**
+     * @expectedException \Mongrate\Exception\InvalidNameException
+     * @expectedExceptionMessage cannot exceed 49 characters, is 61
+     */
+    public function testThrowsExceptionIfTooLong()
+    {
+        new Name('ALongNameThatExceedsTheMaximumLengthXXXXXXXXXXXXXXXXXXXXXXXXX');
+    }
+}
