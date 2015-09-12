efd805fc895835884c6a02e70478e4c0e43f72c1
Amy Boyd <amy@amyboyd.co.uk>
2015-09-12 19:23:04 +0100

    Set application name and version, shown in the console.

--- src/create-application.php
+++ src/create-application.php
@@ -19,3 +19,11 @@ $app->add(new \Mongrate\Command\DownCommand);
 $app->add(new \Mongrate\Command\GenerateMigrationCommand);
 $app->add(new \Mongrate\Command\ListCommand);
 $app->add(new \Mongrate\Command\TestMigrationCommand);
+$app->setName('Mongrate migration tool');
+$app->setVersion(sprintf(
+    '%s (%s)',
+    substr(`git rev-parse HEAD`, 0, 7),
+    date('Y-m-d H:i:s')
+));
+
+// `$app->run()` cannot be called in this file, because this file is included by the tests.
