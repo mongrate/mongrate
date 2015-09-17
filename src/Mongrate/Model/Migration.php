3a80e4ad625fd589f3d38462364a486686a9b8b5
Bernardo Silva <benny.stuff@gmail.com>
2015-09-17 19:37:08 +0100

    Add Migration data model.

---
+++ src/Mongrate/Model/Migration.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Mongrate\Model;
+
+use Mongrate\Migration\Name;
+
+class Migration
+{
+    /**
+     * @var Name
+     */
+    private $name;
+
+    /**
+     * @var boolean
+     */
+    private $isApplied;
+
+    public function __construct(Name $name, $isApplied)
+    {
+        $this->name = $name;
+        $this->isApplied = (bool) $isApplied;
+    }
+
+    /**
+     * @return Name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isApplied()
+    {
+        return $this->isApplied;
+    }
+}
